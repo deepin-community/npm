18ad66fe1f5af75c4fbd87c1d79e03c4a7e9e7ff
Tianyu Chen <sweetyfish@deepin.org>
2023-10-19 01:37:46 +0800

    feat: update npm to 9.2.0_ds1

--- test/fixtures/sandbox.js
+++ test/fixtures/sandbox.js
@@ -3,19 +3,18 @@ const { EventEmitter } = require('events')
 const { homedir, tmpdir } = require('os')
 const { dirname, join } = require('path')
 const { promisify } = require('util')
-const mkdirp = require('mkdirp-infer-owner')
-const npmlog = require('npmlog')
+const { mkdir } = require('fs/promises')
 const rimraf = promisify(require('rimraf'))
-const t = require('tap')
+const mockLogs = require('./mock-logs')
+const pkg = require('../../package.json')
 
-let active = null
 const chain = new Map()
 const sandboxes = new Map()
 
 // keep a reference to the real process
 const _process = process
 
-const processHook = createHook({
+createHook({
   init: (asyncId, type, triggerAsyncId, resource) => {
     // track parentage of asyncIds
     chain.set(asyncId, triggerAsyncId)
@@ -33,19 +32,6 @@ const processHook = createHook({
   },
 }).enable()
 
-for (const level in npmlog.levels) {
-  npmlog[`_${level}`] = npmlog[level]
-  npmlog[level] = (...args) => {
-    process._logs = process._logs || {}
-    process._logs[level] = process._logs[level] || []
-    process._logs[level].push(args)
-    const _level = npmlog.level
-    npmlog.level = 'silent'
-    npmlog[`_${level}`](...args)
-    npmlog.level = _level
-  }
-}
-
 const _data = Symbol('sandbox.data')
 const _dirs = Symbol('sandbox.dirs')
 const _test = Symbol('sandbox.test')
@@ -56,11 +42,10 @@ const _output = Symbol('sandbox.output')
 const _proxy = Symbol('sandbox.proxy')
 const _get = Symbol('sandbox.proxy.get')
 const _set = Symbol('sandbox.proxy.set')
+const _logs = Symbol('sandbox.logs')
 
 // these config keys can be redacted widely
 const redactedDefaults = [
-  'node-version',
-  'npm-version',
   'tmp',
 ]
 
@@ -91,6 +76,7 @@ class Sandbox extends EventEmitter {
       global: options.global || join(tempDir, 'global'),
       home: options.home || join(tempDir, 'home'),
       project: options.project || join(tempDir, 'project'),
+      cache: options.cache || join(tempDir, 'cache'),
     }
 
     this[_proxy] = new Proxy(_process, {
@@ -110,7 +96,7 @@ class Sandbox extends EventEmitter {
   }
 
   get logs () {
-    return this[_proxy]._logs
+    return this[_logs]
   }
 
   get global () {
@@ -123,6 +109,10 @@ class Sandbox extends EventEmitter {
 
   get project () {
     return this[_dirs].project
+  }
+
+  get cache () {
+    return this[_dirs].cache
   }
 
   get process () {
@@ -164,6 +154,8 @@ class Sandbox extends EventEmitter {
       .split(normalize(homedir())).join('{REALHOME}')
       .split(this[_proxy].platform).join('{PLATFORM}')
       .split(this[_proxy].arch).join('{ARCH}')
+      .replace(new RegExp(process.version, 'g'), '{NODE-VERSION}')
+      .replace(new RegExp(pkg.version, 'g'), '{NPM-VERSION}')
 
     // We do the defaults after everything else so that they don't cause the
     // other cleaners to miss values we would have clobbered here.  For
@@ -173,15 +165,15 @@ class Sandbox extends EventEmitter {
     if (this[_npm]) {
       // replace default config values with placeholders
       for (const name of redactedDefaults) {
-        let value = this[_npm].config.defaults[name]
-        clean = clean.split(value).join(`{${name.toUpperCase()}}`)
+        const value = this[_npm].config.defaults[name]
+        clean = clean.split(normalize(value)).join(`{${name.toUpperCase()}}`)
       }
 
       // replace vague default config values that are present within quotes
       // with placeholders
       for (const name of vagueRedactedDefaults) {
         const value = this[_npm].config.defaults[name]
-        clean = clean.split(`"${value}"`).join(`"{${name.toUpperCase()}}"`)
+        clean = clean.split(`"${normalize(value)}"`).join(`"{${name.toUpperCase()}}"`)
       }
     }
 
@@ -202,7 +194,12 @@ class Sandbox extends EventEmitter {
   // test.teardown hook
   teardown () {
     if (this[_parent]) {
+      const sandboxProcess = sandboxes.get(this[_parent])
+      sandboxProcess.removeAllListeners('log')
       sandboxes.delete(this[_parent])
+    }
+    if (this[_npm]) {
+      this[_npm].unload()
     }
     return rimraf(this[_dirs].temp).catch(() => null)
   }
@@ -217,20 +214,7 @@ class Sandbox extends EventEmitter {
       return Reflect.get(this, prop, this)
     }
 
-    const actual = Reflect.get(target, prop, receiver)
-    if (typeof actual === 'function') {
-      // in node 10.1 there's an interesting bug where if a function on process
-      // is called without explicitly forcing the 'this' arg to something, we
-      // get 'Illegal invocation' errors. wrapping function properties in their
-      // own proxy so that we can make sure the context is right fixes it
-      return new Proxy(actual, {
-        apply: (target, context, args) => {
-          return Reflect.apply(target, _process, args)
-        },
-      })
-    }
-
-    return actual
+    return Reflect.get(target, prop, receiver)
   }
 
   // proxy set handler
@@ -255,9 +239,9 @@ class Sandbox extends EventEmitter {
 
   async run (command, argv = []) {
     await Promise.all([
-      mkdirp(this.project),
-      mkdirp(this.home),
-      mkdirp(this.global),
+      mkdir(this.project, { recursive: true }),
+      mkdir(this.home, { recursive: true }),
+      mkdir(this.global, { recursive: true }),
     ])
 
     // attach the sandbox process now, doing it after the promise above is
@@ -273,37 +257,30 @@ class Sandbox extends EventEmitter {
       '--prefix', this.project,
       '--userconfig', join(this.home, '.npmrc'),
       '--globalconfig', join(this.global, 'npmrc'),
+      '--cache', this.cache,
       command,
       ...argv,
     ]
 
-    this[_npm] = this[_test].mock('../../lib/npm.js', this[_mocks])
+    const mockedLogs = mockLogs(this[_mocks])
+    this[_logs] = mockedLogs.logs
+    const Npm = this[_test].mock('../../lib/npm.js', {
+      '../../lib/utils/update-notifier.js': async () => {},
+      ...this[_mocks],
+      ...mockedLogs.logMocks,
+    })
+    this.process.on('log', (l, ...args) => {
+      if (l !== 'pause' && l !== 'resume') {
+        this[_logs].push([l, ...args])
+      }
+    })
+
+    this[_npm] = new Npm()
     this[_npm].output = (...args) => this[_output].push(args)
     await this[_npm].load()
-    // in some node versions (later 10.x) our executionAsyncId at this point
-    // will for some reason appear to have been triggered by a different parent
-    // so immediately after load, if we can see that we lost our ancestry, we
-    // fix it here with a hammer
-    if (chain.get(executionAsyncId()) !== this[_parent]) {
-      chain.set(executionAsyncId(), this[_parent])
-      process = this[_proxy]
-    }
 
     const cmd = this[_npm].argv.shift()
-    const impl = this[_npm].commands[cmd]
-    if (!impl) {
-      throw new Error(`Unknown command: ${cmd}`)
-    }
-
-    return new Promise((resolve, reject) => {
-      impl(this[_npm].argv, (err) => {
-        if (err) {
-          return reject(err)
-        }
-
-        return resolve()
-      })
-    })
+    return this[_npm].exec(cmd, this[_npm].argv)
   }
 
   async complete (command, argv, partial) {
@@ -313,9 +290,9 @@ class Sandbox extends EventEmitter {
     }
 
     await Promise.all([
-      mkdirp(this.project),
-      mkdirp(this.home),
-      mkdirp(this.global),
+      mkdir(this.project, { recursive: true }),
+      mkdir(this.home, { recursive: true }),
+      mkdir(this.global, { recursive: true }),
     ])
 
     // attach the sandbox process now, doing it after the promise above is
@@ -331,27 +308,29 @@ class Sandbox extends EventEmitter {
       '--prefix', this.project,
       '--userconfig', join(this.home, '.npmrc'),
       '--globalconfig', join(this.global, 'npmrc'),
+      '--cache', this.cache,
       command,
       ...argv,
     ]
 
-    this[_npm] = this[_test].mock('../../lib/npm.js', this[_mocks])
+    const mockedLogs = mockLogs(this[_mocks])
+    this[_logs] = mockedLogs.logs
+    const Npm = this[_test].mock('../../lib/npm.js', {
+      '../../lib/utils/update-notifier.js': async () => {},
+      ...this[_mocks],
+      ...mockedLogs.logMocks,
+    })
+    this.process.on('log', (l, ...args) => {
+      if (l !== 'pause' && l !== 'resume') {
+        this[_logs].push([l, ...args])
+      }
+    })
+
+    this[_npm] = new Npm()
     this[_npm].output = (...args) => this[_output].push(args)
     await this[_npm].load()
-    // in some node versions (later 10.x) our executionAsyncId at this point
-    // will for some reason appear to have been triggered by a different parent
-    // so immediately after load, if we can see that we lost our ancestry, we
-    // fix it here with a hammer
-    if (chain.get(executionAsyncId()) !== this[_parent]) {
-      chain.set(executionAsyncId(), this[_parent])
-      process = this[_proxy]
-    }
 
-    const impl = this[_npm].commands[command]
-    if (!impl) {
-      throw new Error(`Unknown command: ${cmd}`)
-    }
-
+    const impl = await this[_npm].cmd(command)
     return impl.completion({
       partialWord: partial,
       conf: {
